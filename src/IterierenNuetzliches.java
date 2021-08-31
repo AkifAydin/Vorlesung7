866e87f83c53fc5ce8d654ae6c54a1d5f9a24ba8
Akif Aydin <akifnidya@hotmail.de>
2021-08-31 21:33:21 +0200

    update

---
+++ src/IterierenNuetzliches.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+public class IterierenNuetzliches {
+    public static void main(String[] args) {
+
+        Map<Integer, Integer> mii =
+                Map.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        mii.forEach((key, val) ->
+                System.out.println(key + "->" + val));
+        List<Integer> li =
+                Arrays.asList(1, 2, 3, 4, 5, 6, 8, 9);
+        li.forEach(elem ->
+                System.out.println(elem));
+    }
+}
--- src/Streaming.java
+++ src/Streaming.java
@@ -1,6 +1,5 @@
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Streaming {
@@ -12,9 +11,14 @@ public class Streaming {
     public static void main(String[] args) {
 
         System.out.println("Die Elemente Quadrieren");
-        Double[] nums = { 1.0, 2.0, 3.0, 4.0, 5.0 };
+        Double[] nums = { 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0 };
         Double[] squares = Stream.of(nums).map(e -> e * e).toArray(Double[]::new);
         System.out.println(Arrays.toString(squares));
+
+        System.out.println("von stelle 3.0 bis 5.0");
+        Double[] num = { 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0 };
+        Double[] square = Stream.of(num).map(e -> e * e).limit(5).skip(2).toArray(Double[]::new);
+        System.out.println(Arrays.toString(square));
 
         System.out.println("nur Elemente größer als 2.0 in neues Array");
         Double[] bereich = Stream.of(nums).filter(i-> i>2.0).toArray(Double[]::new);
@@ -23,6 +27,15 @@ public class Streaming {
         System.out.println("nur erstes Element");
         Double ersteZahl = Stream.of(nums).findFirst().get();
         System.out.println(ersteZahl);
+
+        System.out.println("Duplikate innerhalb eines Arrays entfernen");
+        Integer[] noDups = Stream.of(new Integer[]
+                { 1, 2, 3, 4, 1, 5, 6, 3, 4, 1, 2 }).distinct().toArray(Integer[]::new);
+        System.out.println(Arrays.toString(noDups));
+
+        System.out.println("Mithilfe eines Intstreams random werte generieren in einem wertebereich");
+        IntStream infinit = new Random().ints().limit(10);
+        System.out.println(Arrays.toString(infinit.toArray()));
 
     }
 }
